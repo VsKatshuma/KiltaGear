506ef04899c30e5def5bc80bf54ef7428bd84383
Esa Koskinen <esakoskin2@gmail.com>
2019-10-06 12:34:59 +0300

    Fix game loop TS errors, runs correctly now

--- src/gameloop.ts
+++ src/gameloop.ts
@@ -1,5 +1,5 @@
 import * as kiltagear from './kiltagear'
-import { GameScreen, Player } from './types';
+import { GameScreen, Player, ActiveAttack, InputStatus } from './types';
 
 // As a developer, I want this file to be indented with 2 spaces. -- Esa
 
@@ -7,6 +7,7 @@ const FRAMES_PER_SECOND = 60
 type GameState = {
   screen: GameScreen,
   players?: Player[],
+  activeAttacks: ActiveAttack[],
   frame: number
 }
 
@@ -14,6 +15,7 @@ export const startGameLoop = () => {
   let currentState: GameState = {
     screen: 'title-screen',
     players: kiltagear.players,
+    activeAttacks: [],
     frame: 0
   }
   const interval = window.setInterval(() => {
@@ -22,36 +24,41 @@ export const startGameLoop = () => {
 }
 
 // Functional loop: return next state from current state and inputs
-const nextState = (currentState, inputs): GameState => {
-  console.log(`advance frame:\n  currentState: ${currentState}\n  inputs: ${inputs}`)
+const nextState = (currentState, inputs: InputStatus): GameState => {
+  // console.log('advance frame:\n  currentState:', currentState, '\n  inputs: ', inputs)
 
   switch (currentState.screen) {
     case 'in-game':
       // TODO
-
-      return {
-        screen: 'title-screen',
-        players: currentState.players,
-        frame: currentState.frame + 1
+      if (inputs[' '] && inputs[' '].isDown) {
+        return {
+          ...currentState,
+          screen: 'title-screen',
+          frame: currentState.frame + 1
+        }
       }
       break
     case 'character-select':
-      return {
-        screen: 'in-game',
-        players: currentState.players,
-        frame: currentState.frame + 1
+      if (inputs[' '] && inputs[' '].isDown) {
+        return {
+          ...currentState,
+          screen: 'in-game',
+          frame: currentState.frame + 1
+        }
       }
       break
     case 'title-screen':
-      return {
-        screen: 'character-select',
-        players: currentState.players,
-        frame: currentState.frame + 1
+      if (inputs[' '] && inputs[' '].isDown) {
+        return {
+          ...currentState,
+          screen: 'character-select',
+          frame: currentState.frame + 1
+        }
       }
       break
     default:
       throw new Error(`unknown game state when pressing key\n  state: ${currentState}\n  key event: ${event}`)
   }
-  return // TODO
+  return currentState
 }
 
