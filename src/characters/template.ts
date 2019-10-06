a1e44e05b51a11c6ad813c6ad0db9eeaaad46ced
Esa Koskinen <esakoskin2@gmail.com>
2019-10-06 12:02:13 +0300

    Make character files contain data instead of functions

--- src/characters/template.ts
+++ src/characters/template.ts
@@ -2,15 +2,14 @@ import { createHitbox, generateAttack } from "../kiltagear";
 import { Character } from "../types";
 
 
-export default (): Character => {
-  return {
-    health: 100,
-    walkSpeed: 5,
-    airSpeed: 8,
-    weight: 1,
-    jumpStrength: 1,
-
-    lightNeutral: {
+export const mmKALLL: Character = {
+  health: 100,
+  walkSpeed: 5,
+  airSpeed: 8,
+  weight: 1,
+  jumpStrength: 1,
+  attacks: {
+    LightNeutral: {
       ...generateAttack([
         { ...createHitbox(4, 12, 5), radius: 123 },
         { ...createHitbox(12, 20, 10) },
