790d132ec6595bbf605d781f195097f466704620
VsKatshuma <vskatshuma@gmail.com>
2019-10-07 21:22:41 +0300

    Fixed characters having one more jump than intended

--- src/game-logic/physics.ts
+++ src/game-logic/physics.ts
@@ -18,13 +18,14 @@ const nextPlayers = (players: Player[]): Player[] => {
   return players.map((player) => {
     let nextY = Math.min(600, player.y + player.ySpeed)
     let nextYSpeed = nextY >= 600 ? 0 : Math.min(18, player.ySpeed + 0.6)
+    let nextJumps = player.y < 600 && nextY >= 600 ? player.character.maxJumps : player.jumps
     return {
       ...player,
       x: Math.max(player.character.hurtboxRadius, Math.min(1200 - player.character.hurtboxRadius, player.x + player.xSpeed)),
       y: nextY,
       ySpeed: nextYSpeed,
       xSpeed: Math.abs(player.xSpeed) < 0.3 ? 0 : player.xSpeed * 0.86,
-      jumps: player.y >= 600 ? player.character.maxJumps : player.jumps,
+      jumps: nextJumps,
     }
   })
 
