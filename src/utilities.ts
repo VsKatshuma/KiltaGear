9cf45c722f6faf6d647359f437bf45e0bcabbe47
Esa Koskinen <esakoskin2@gmail.com>
2020-05-25 16:03:27 +0900

    add a sum helper in utilities.ts
    - needed to sum through the x-differences in Katshuma's meter gain

--- src/utilities.ts
+++ src/utilities.ts
@@ -7,6 +7,10 @@ export const clamp = (number: number, min: number, max: number): number => {
   return Math.min(max, Math.max(min, number))
 }
 
+export const sum = (array: number[]): number => {
+  return array.reduce((a, b) => a + b)
+}
+
 // Explicitly check that all inferred types are used - see e.g. game-loop.ts
 export function assertNever(x: never): never {
   throw new Error(`Unexpected object in assertNever:\n  ${x}`);
